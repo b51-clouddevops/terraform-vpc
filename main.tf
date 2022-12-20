e271cadaf0a6b820c5ba504f57709db126c9ca60
mvpc-11232 <mvp2612@hotmail.com>
2022-12-20 07:58:30 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source                = "./vendor/modules/vpc"  
-  VPC_CIDR              = var.VPC_CIDR
-  ENV                   = var.ENV
-  PUBLIC_SUBNET_CIDR    = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR   = var.PRIVATE_SUBNET_CIDR
-  AZ                    = var.AZ 
-  DEFAULT_VPC_ID        = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR      = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT        = var.DEFAULT_VPC_RT
-  PRIVATE_HOSTED_ZONEID = var.PRIVATE_HOSTED_ZONEID
-  PRIVATE_HOSTED_ZONENAME = var.PRIVATE_HOSTED_ZONENAME 
-  PUBLIC_HOSTED_ZONEID    = var.PUBLIC_HOSTED_ZONEID
-  
+  source                   = "./vendor/modules/vpc"  
+  VPC_CIDR                 = var.VPC_CIDR
+  ENV                      = var.ENV
+  PUBLIC_SUBNET_CIDR       = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR      = var.PRIVATE_SUBNET_CIDR
+  AZ                       = var.AZ 
+  DEFAULT_VPC_ID           = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR         = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT           = var.DEFAULT_VPC_RT
+  PRIVATE_HOSTED_ZONEID    = var.PRIVATE_HOSTED_ZONEID
+  PRIVATE_HOSTED_ZONENAME  = var.PRIVATE_HOSTED_ZONENAME 
+  PUBLIC_HOSTED_ZONEID     = var.PUBLIC_HOSTED_ZONEID
+  PUBLIC_HOSTED_ZONENAME   = var.PUBLIC_HOSTED_ZONENAME
 }
 
 # module "vpc" {
@@ -22,6 +22,3 @@ module "vpc" {
 # How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
 # Alternatives to make it flexible ?
 
-variable "PRIVATE_HOSTED_ZONENAME" {}
-variable "PUBLIC_HOSTED_ZONEID" {}
-variable "PUBLIC_HOSTED_ZONENAME" {}
